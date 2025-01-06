17ecf86779dd5b57557d552c55bd227ff86b9feb
Vje <vje783next@gmail.com>
2025-01-06 12:09:11 +0100

    commit 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      screens: {
+        'tablet': '800px',
+        'laptop': '1200px',
+      },
       colors: {
         'primary': '#5948de',
         'secondary': '#311550',
@@ -14,20 +17,24 @@ module.exports = {
         'purple': '#CC2DE1',
       },
       fontSize: {
-        '2': '2rem',
-        '1.6': '1.6rem',
+        '1.2': '1.2rem',
+        '1.4': '1.4rem',
+        '1.5': '1.5rem',
         '1.7': '1.75rem',
-        '2.8': '2.8rem',  
-  
+        '2': '2rem',
       },
       fontFamily: {
         verdana: ['Verdana', 'sans-serif'], // Fuente personalizada
       },
       boxShadow: {
         custom: '1px 1px 10px #80FF00',
-      }
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
     },
+
   },
-  plugins: [],
-}
+  plugins: [
+  ],}
 
