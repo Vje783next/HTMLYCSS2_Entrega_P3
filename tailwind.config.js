e0df4c058c210d8adf93f5724f5a30ee754bdc23
Vje <vje783next@gmail.com>
2025-01-18 23:11:30 +0100

    commit last

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,16 @@ module.exports = {
       },
       fontSize: {
         '1.2': '1.2rem',
+        '1.35': '.1.35rem',
         '1.4': '1.4rem',
         '1.5': '1.5rem',
+        '1.6': '1.6rem',
         '1.7': '1.75rem',
         '2': '2rem',
+        '2.8': '2.8rem',
       },
       fontFamily: {
-        verdana: ['Verdana', 'sans-serif'], // Fuente personalizada
+        verdana: ['Verdana', 'sans-serif'],
       },
       boxShadow: {
         custom: '1px 1px 10px #80FF00',
